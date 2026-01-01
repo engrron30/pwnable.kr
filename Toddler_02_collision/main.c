f59927663e17972445f255494464511eac3ce241
engrron30 <ronaldtorresrosete30@gmail.com>
2026-01-02 00:18:47 +0800

    Add comments for "1234"

--- Toddler_02_collision/main.c
+++ Toddler_02_collision/main.c
@@ -3,9 +3,21 @@
 
 int main()
 {
+	/* Start with a character array of 1234 
+	 *
+	 * CHARACTER | ASCII (hex) | ASCII (dec)
+	 * ----------+-------------+------------
+	 *    '1'    |    0x31     |    49
+	 *    '2'    |    0x32     |    50
+	 *    '3'    |    0x33     |    51
+	 *    '4'    |    0x34     |    52
+	 * */
 	const char *cp = "1234";
+
 	int *ip = (int *) cp;
 
+	// Printing the string "1234" will be 
 	printf("ip = %d\n", *ip);
+
 	return 0;
 }
