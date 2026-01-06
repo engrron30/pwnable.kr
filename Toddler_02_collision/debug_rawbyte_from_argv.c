9d5c43f00d67165d541476d66eb825c1f6f91fd9
engrron30 <ronaldtorresrosete30@gmail.com>
2026-01-06 09:09:28 +0800

    Add defines when running the binary

--- Toddler_02_collision/debug_rawbyte_from_argv.c
+++ Toddler_02_collision/debug_rawbyte_from_argv.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_ARGC_NUM			2
+#define BIN_NAME_ARGC_NUM		0
+#define RAW_BYTE_ARGC_NUM		1
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +12,8 @@ int main(int argc, char *argv[])
 		printf("usage: debug <raw-bytes>\n");
 		return 1;
 	}
+
+	printf("raw_byte: 0x%x (%d)\n", argv[RAW_BYTE_ARGC_NUM], atoi(argv[RAW_BYTE_ARGC_NUM]));
 
 	return 0;
 }
